1cec84a1fdb24bb2d1a48f3214e43d9c5e80df20
hugo <youngkwang.hugo@gmail.com>
2024-04-15 12:11:15 +0900

    html커밋

--- src/main/java/com/mydata/authlogin/security/AuthService.java
+++ src/main/java/com/mydata/authlogin/security/AuthService.java
@@ -22,82 +22,91 @@ import com.mydata.authlogin.mapper.MemberMapper;
 
 @Service
 // public class AuthService extends DefaultOAuth2UserService  {
-public class AuthService extends DefaultOAuth2UserService implements UserDetailsService {
+// public class AuthService extends DefaultOAuth2UserService  {
+    public class AuthService extends DefaultOAuth2UserService implements UserDetailsService {
     
-    @Autowired
-    MemberMapper memberMapper;
-
-    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-    // UserDetails 자체 로그인
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-       System.out.println("디테일즈"+username);
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
-    }
-
-    // OAUTH 로그인
-    @SuppressWarnings("unchecked")
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-      
-        String social = (String)userRequest.getClientRegistration().getRegistrationId();
-        String email ="";
-        Member member = new Member();
+        @Autowired
+        MemberMapper memberMapper;
+    
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    
+        // UserDetails 자체 로그인
+        @Override
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+           System.out.println("디테일즈"+username);
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+        }
+    
+        // OAUTH 로그인
+        @SuppressWarnings("unchecked")
+        @Override
+        public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+          
+            String social = (String)userRequest.getClientRegistration().getRegistrationId();
+            String email ="";
+            Member member = new Member();
+            
+            Map<String,Object> atteributes = new HashMap<>();
+    
+            if(social.contains("google")){
+                email = (String)super.loadUser(userRequest).getAttributes().get("email");
+                social = "google";
+                atteributes =super.loadUser(userRequest).getAttributes();
+    
+            }else if(social.contains("naver")){
+                atteributes = (Map<String, Object>) super.loadUser(userRequest).getAttributes().get("response");
+                email = (String)atteributes.get("email");
+                social = "naver";
+    
+            }else if(social.contains("kakao")){
+                atteributes = (Map<String, Object>)super.loadUser(userRequest).getAttributes().get("kakao_account");
+                atteributes.put("id", (Object)super.loadUser(userRequest).getAttributes().get("id"));
+                email = (String)atteributes.get("email");
+                social = "kakao";
+            }
+    
+            member = memberMapper.findMemberByEmail(email);
+    
+            if(member == null){
+                member = collectMember(social,atteributes);
+                int status = memberMapper.memberJoin(member);
+                
+                
+            }
+    
+            
+            //authUser 리턴
+            return new AuthUser(member,super.loadUser(userRequest).getAttributes());
+        }
         
-        Map<String,Object> atteributes = new HashMap<>();
-
-        if(social.contains("google")){
-            email = (String)super.loadUser(userRequest).getAttributes().get("email");
-            social = "google";
-            atteributes =super.loadUser(userRequest).getAttributes();
-
-        }else if(social.contains("naver")){
-            atteributes = (Map<String, Object>) super.loadUser(userRequest).getAttributes().get("response");
-            email = (String)atteributes.get("email");
-            social = "naver";
-
-        }else if(social.contains("kakao")){
-            System.out.println("카카오");
-            
-        }
-
-        member = memberMapper.findMemberByEmail(email);
-
-        if(member == null){
-            member = collectMember(social,atteributes);
-            int status = memberMapper.memberJoin(member);
-            // int status = memberMapper.memberJoin2("member");
-            
-        }
-
-
-        //authUser 리턴
-        return new AuthUser(member,super.loadUser(userRequest).getAttributes());
-    }
     
-
+        
+        private Member collectMember(String social, Map<String,Object>atteributes){
+            Member member = null;
+            if(social.contains("google")){
+                String passward = encoder.encode((String)atteributes.get("sub"));
+                member = Member.builder()
+                    .email((String)atteributes.get("email")).name((String)atteributes.get("name"))
+                    .passward(passward).authSocial((String)social).build();
+                
     
-    private Member collectMember(String social, Map<String,Object>atteributes){
-        Member member = null;
-        if(social.contains("google")){
-            String passward = encoder.encode((String)atteributes.get("sub"));
-            member = Member.builder()
+            }else if(social.contains("naver")){
+                String passward = encoder.encode((String)atteributes.get("id"));
+                member = Member.builder()
                 .email((String)atteributes.get("email")).name((String)atteributes.get("name"))
                 .passward(passward).authSocial((String)social).build();
-            
-
-        }else if(social.contains("naver")){
-            String passward = encoder.encode((String)atteributes.get("id"));
-            member = Member.builder()
-            .email((String)atteributes.get("email")).name((String)atteributes.get("name"))
-            .passward(passward).authSocial((String)social).build();
-          
-        }else if(social.contains("kakao")){
-            // System.out.println("카카오");
+              
+            }else if(social.contains("kakao")){
+                String passward = encoder.encode(Long.toString((Long)atteributes.get("id")));
+                @SuppressWarnings("unchecked")
+                Map<String,Object> nickName = (Map<String,Object>)atteributes.get("profile");
+                
+                member =Member.builder()
+                    .email((String)atteributes.get("email")).name((String)nickName.get("nickname")).passward(passward).authSocial(social).build();
+            }
+            System.out.println(member.toString());
+            return member;
         }
-        System.out.println(member.toString());
-        return member;
     }
-}
+    
